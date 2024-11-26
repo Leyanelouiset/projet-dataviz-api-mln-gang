c87ef3e5bd6fdc21756476f2735f0afe8b61ee90
Marie Marchal <you@example.com>
2024-11-26 16:01:06 +0100

    dataAPI

---
+++ script.js
@@ -0,0 +1,23 @@
+//const airQuality = fetch(`https://api.waqi.info/feed/${city}/?token=d3e053b50d1e553a6dcbbcb72a09af72a9f54878`)
+let input = document.querySelector("#valeur")
+
+let bouton = document.querySelector("#valider")
+
+let city = ""
+
+bouton.addEventListener("click", () => {
+    city = input.value
+    console.log("test", city)
+    getAirQuality(city)
+})
+
+
+async function getAirQuality() {
+    const airQuality = await fetch (`https://api.waqi.info/feed/${city}/?token=d3e053b50d1e553a6dcbbcb72a09af72a9f54878`)
+    const indexAir = await airQuality.json()
+    console.log ("test API", indexAir)
+}
+//let result =getAirQuality()
+
+
+
