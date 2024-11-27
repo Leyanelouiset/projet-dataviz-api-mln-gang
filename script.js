32a994936e7479c94cf5dae6841cf7fb67aedbd3
nicordo19 <poiraudnico@gmail.com>
2024-11-27 14:50:53 +0100

    commit carré

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //const airQuality = fetch(`https://api.waqi.info/feed/${city}/?token=d3e053b50d1e553a6dcbbcb72a09af72a9f54878`)
 let inputCity = document.querySelector("#valeurCity");
 let bouton = document.querySelector("#valider");
+let polution = document.querySelector("#container");
 let city = "";
 
 bouton.addEventListener("click", () => {
@@ -9,12 +10,23 @@ bouton.addEventListener("click", () => {
   getAirQuality(city);
 });
 
-
-
 async function getAirQuality() {
-  const airQuality = await fetch(`https://api.waqi.info/feed/${city}/?token=d3e053b50d1e553a6dcbbcb72a09af72a9f54878`);
+  const airQuality = await fetch(
+    `https://api.waqi.info/feed/${city}/?token=d3e053b50d1e553a6dcbbcb72a09af72a9f54878`
+  );
   const indexAir = await airQuality.json();
   console.log("test API", indexAir.data.aqi);
+  if (indexAir.data.aqi <= 100) {
+    polution.style.width = "60px";
+    polution.style.height = "100px";
+    polution.style.backgroundColor = "green";
+  } else if (indexAir.data.aqi <= 200) {
+    polution.style.width = "60px";
+    polution.style.height = "100px";
+    polution.style.backgroundColor = "red";
+  } else if (indexAir.data.aqi > 200) {
+    polution.style.width = "60px";
+    polution.style.height = "100px";
+    polution.style.backgroundColor = "purple";
+  }
 }
-
-
