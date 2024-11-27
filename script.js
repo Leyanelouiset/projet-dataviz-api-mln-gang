74b85a60ecf8c8907aca2e59a9fc30615e7ee5e4
leyanelouiset <louisetleyane@gmail.com>
2024-11-27 14:07:29 +0100

    add AQI

--- script.js
+++ script.js
@@ -9,11 +9,16 @@ bouton.addEventListener("click", () => {
   getAirQuality(city);
 });
 
+
+
 async function getAirQuality() {
   const airQuality = await fetch(
     `https://api.waqi.info/feed/${city}/?token=d3e053b50d1e553a6dcbbcb72a09af72a9f54878`
   );
   const indexAir = await airQuality.json();
-  console.log("test API", indexAir);
+  console.log("test API", indexAir.data.aqi);
+
+ 
 }
-//let result =getAirQuality()
+
+
