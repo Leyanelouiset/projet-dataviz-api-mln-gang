143602cef2d58b0c3b8dad14a2106dd764668c62
Marie Marchal <you@example.com>
2024-11-27 10:57:02 +0100

    miseajour

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-P; //const airQuality = fetch(`https://api.waqi.info/feed/${city}/?token=d3e053b50d1e553a6dcbbcb72a09af72a9f54878`)
-let input = document.querySelector("#valeur");
+//const airQuality = fetch(`https://api.waqi.info/feed/${city}/?token=d3e053b50d1e553a6dcbbcb72a09af72a9f54878`)
+let inputCity = document.querySelector("#valeurCity");
 let bouton = document.querySelector("#valider");
 let city = "";
 
 bouton.addEventListener("click", () => {
-  city = input.value;
+  city = inputCity.value;
   console.log("test", city);
   getAirQuality(city);
 });
 
 async function getAirQuality() {
-  const airQuality = await fetch(
-    `https://api.waqi.info/feed/${city}/?token=d3e053b50d1e553a6dcbbcb72a09af72a9f54878`
-  );
+  const airQuality = await fetch(`https://api.waqi.info/feed/${city}/?token=d3e053b50d1e553a6dcbbcb72a09af72a9f54878`);
   const indexAir = await airQuality.json();
   console.log("test API", indexAir);
 }
