0d0e750c1bea9fcd0ddc1c71ab82877b97c73626
nicordo19 <poiraudnico@gmail.com>
2024-11-28 16:49:12 +0100

    je recupere les informations de la consomation de l'éléctricité en kWt par an

--- script.js
+++ script.js
@@ -9,10 +9,13 @@ bouton.addEventListener("click", () => {
   console.log("test", city);
   getAirQuality(city);
   getWaterQuality(city);
+  getElectricQuality(city);
 });
 
 async function getAirQuality() {
-  const airQuality = await fetch(`https://api.waqi.info/feed/${city}/?token=d3e053b50d1e553a6dcbbcb72a09af72a9f54878`);
+  const airQuality = await fetch(
+    `https://api.waqi.info/feed/${city}/?token=d3e053b50d1e553a6dcbbcb72a09af72a9f54878`
+  );
   const indexAir = await airQuality.json();
   console.log("test air", indexAir.data.aqi);
   if (indexAir.data.aqi <= 100) {
@@ -31,12 +34,30 @@ async function getAirQuality() {
 }
 
 async function getWaterQuality() {
-  const waterQuality = await fetch (`https://hubeau.eaufrance.fr/api/v1/qualite_eau_potable/resultats_dis?nom_commune=${city}`);
+  const waterQuality = await fetch(
+    `https://hubeau.eaufrance.fr/api/v1/qualite_eau_potable/resultats_dis?nom_commune=${city}`
+  );
   const indexEau = await waterQuality.json();
-  console.log("test eau", indexEau.data[0].conformite_limites_bact_prelevement)
-  if (indexEau.data[0].conformite_limites_bact_prelevement == "C" && indexEau.data[0].conformite_limites_pc_prelevement == "C" && indexEau.data[0].conformite_references_bact_prelevement == "C" && indexEau.data[0].conformite_references_pc_prelevement == "C") {
-    polution.innerText = "Eau conforme !!"
+  console.log("test eau", indexEau.data[0].conformite_limites_bact_prelevement);
+  if (
+    indexEau.data[0].conformite_limites_bact_prelevement == "C" &&
+    indexEau.data[0].conformite_limites_pc_prelevement == "C" &&
+    indexEau.data[0].conformite_references_bact_prelevement == "C" &&
+    indexEau.data[0].conformite_references_pc_prelevement == "C"
+  ) {
+    polution.innerText = "Eau conforme !!";
   } else {
-    polution.innerText = "Eau non potable !!"
+    polution.innerText = "Eau non potable !!";
   }
+}
+async function getElectricQuality(city) {
+  console.log("je suis dans getElectricQuality");
+  const url = `https://opendata.agenceore.fr/api/explore/v2.1/catalog/datasets/conso-elec-gaz-annuelle-par-secteur-dactivite-agregee-commune/records?limit=20&refine=libelle_commune%3A${city}`;
+  console.log("url", url);
+  const indexElectric = await fetch(url);
+  const electric = await indexElectric.json();
+  console.log(
+    "je suis dans getElectricQuality, je regarde electric:",
+    electric.results[0].consototale
+  );
 }
